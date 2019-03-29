d2f93f6d2c8f3f1b38336ec6a30aeb539dbcd811
TienCGC8JAVA <nguyenmanhtien08081994@gmail.com>
2019-03-29 10:19:29 +0700

    Sự kiện bàn phím

---
+++ ClickMouse.js
@@ -0,0 +1,15 @@
+var myImg = document.getElementById("funny");
+
+function moveRightImg()
+{
+    myImg.style.left = parseInt(myImg.style.left) + 10 +'px';
+}
+
+function click()
+{
+    myImg = document.getElementById("funny");
+    myImg.style.position = "relative";
+    myImg.style.left = "0px";
+}
+
+window.onload = click;
