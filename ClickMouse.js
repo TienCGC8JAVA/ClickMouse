6ca7b364e31a292bdbd193e6cff42856eca36620
TienCGC8JAVA <nguyenmanhtien08081994@gmail.com>
2019-03-30 11:08:44 +0700

    Sự kiện chuột

--- ClickMouse.js
+++ ClickMouse.js
@@ -3,10 +3,10 @@ function moveRightImg()
     document.getElementById("funny").style.left = parseInt(document.getElementById("funny").style.left) + 10 +'px';
 }
 
-function click()
+window.onload = function ()
 {
     document.getElementById("funny").style.position = "relative";
     document.getElementById("funny").style.left = "0px";
 }
 
-window.onload = click;
+
